1df85d4afee45a6a75544cdad4654c517ad04fe7
heheheha <133107610+imnotj1w4n@users.noreply.github.com>
2024-05-08 18:53:44 +0900

    Add files via upload

---
+++ 호칭은 멋집니다.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<cstring>
+#include<iostream>
+using namespace std; //난 멋지다 후하하  
+
+int main()	 
+{	 
+string m;
+string y; 
+
+cin >> m;
+cin >> y;
+
+if (m.size()>y.size())
+
+
+
+	
+	
+	
+	
+	
+
+return 0;}
